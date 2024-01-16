50c4741cad7aa0f9ef0e3a77111e5a5304425d9b
Ugo <okekeugochukwu.dev@gmail.com>
2024-01-16 04:52:13 +0100

    Feats: Add projects & skills, persist theme & tab states

--- js/script.js
+++ js/script.js
@@ -1,50 +1,73 @@
-const THEME_STORAGE_KEY = 'theme'
+const constants = {
+   THEME_STORAGE_KEY: 'theme',
+   TAB_STORAGE_KEY: 'tab'
+}
 const htmlEl = document.querySelector("html")
 
-const REGISTER_THEME_MODE = () => {
-   const theme = localStorage.getItem(THEME_STORAGE_KEY);
+const tab = {
+   active: 'projects',
+   skills: document.getElementById('skills'),
+   projects: document.getElementById('projects')
+}
 
-   //check if it's first time visit, then act accordingly 
-   if (theme === null) {
 
-      // if it is, check the device prefrence and match app theme to it. Save in memory for next visit
-      if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-         SET_DARK_MODE()
+/**
+ * Get & set local state
+ */
+
+const REGISTER_CONSTANTS = () => {
+
+   for (const constant in constants) {
+      const localConstant = localStorage.getItem(constants[constant])
+
+      //check if it's first time visit
+      if (localConstant === null) {
+         if (constant[constant] === 'theme') {
+            REGISTER_THEME_MODE()
+         }
       }
       else {
-         SET_LIGHT_MODE()
+         constants[constant] === 'theme' 
+            ? htmlEl.classList.add(localConstant)
+            : REGISTER_TAB(localConstant)
       }
-
-   } else {
-      //if it's not, check saved app theme and set
-      htmlEl.classList.add(theme);
    }
+}
 
-   console.log(localStorage.getItem(THEME_STORAGE_KEY));
+
+/**
+ *  Theming & control
+ */
+
+const REGISTER_THEME_MODE = () => {
+
+   // check the device prefrence and match app theme to it. Save in memory for next visit
+   if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      SET_DARK_MODE()
+   }
+   else {
+      SET_LIGHT_MODE()
+   }
+   
 }
 
 const SET_DARK_MODE = () => {
-   if(htmlEl.classList.contains('light')) {
+   if (htmlEl.classList.contains('light')) {
       htmlEl.classList.remove('light')
    }
 
    htmlEl.classList.add("dark");
-   localStorage.setItem(THEME_STORAGE_KEY, "dark");
-
-   console.log(localStorage.getItem(THEME_STORAGE_KEY, "light"));
+   localStorage.setItem(constants.THEME_STORAGE_KEY, "dark");
 }
 
 const SET_LIGHT_MODE = () => {
-   if(htmlEl.classList.contains('dark')) {
+   if (htmlEl.classList.contains('dark')) {
       htmlEl.classList.remove('dark')
    }
 
    htmlEl.classList.add("light");
-   localStorage.setItem(THEME_STORAGE_KEY, "light");   
-
-   console.log(localStorage.getItem(THEME_STORAGE_KEY, "light"));
+   localStorage.setItem(constants.THEME_STORAGE_KEY, "light");
 }
-document.onload = REGISTER_THEME_MODE()
 
 const SWITCH_MODE = (ipnut) => {
    ipnut.checked
@@ -52,3 +75,51 @@ const SWITCH_MODE = (ipnut) => {
       : SET_DARK_MODE()
 }
 
+
+/**
+ * Tabs control
+ */
+
+const SWITCH_TAB = (incomingTab) => {
+
+   console.log('assd');
+   if (incomingTab === 'projects') {
+      //update state
+      tab.active = 'projects'
+
+      SHOW_PROJECTS_TAB()
+   }
+   else {
+      //upate state
+      tab.active = 'skills'
+
+      SHOW_SKILLS_TAB()
+   }
+}
+
+const REGISTER_TAB = (tab) => {
+   SWITCH_TAB(tab) 
+}
+
+const SHOW_PROJECTS_TAB = () => {   
+   console.log('pre');
+   //upate DOM
+   tab.skills.classList.remove('shown')
+   tab.projects.classList.add('shown')
+
+   //update localstorage
+   localStorage.setItem(constants.TAB_STORAGE_KEY, 'projects')
+}
+
+const SHOW_SKILLS_TAB = () => {
+   //update DOM
+   tab.projects.classList.remove('shown')
+   tab.skills.classList.add('shown')
+
+   //update localstorage
+   localStorage.setItem(constants.TAB_STORAGE_KEY, 'skills')
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+   REGISTER_CONSTANTS()
+}, false);
